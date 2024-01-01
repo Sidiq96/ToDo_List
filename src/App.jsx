b17c440ea80f2eff1b852098823c4b6137af983d
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-01 16:39:35 +0000

    adds a label with checkbox and adds a delete button

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import "./styles.css";
 export default function App() {
   return (
     <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="Item">New Item</label>
-        <input type="text" id="Item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
-     <h1 className="header"> To Do List</h1>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="Item">New Item</label>
+          <input type="text" id="Item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header"> To Do List</h1>
+      <ul className="list">
+        <li>
+          <label type="checkbox">
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
